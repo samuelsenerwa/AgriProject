e34e069cfbf70f3d4f60f002077784ac33d7a38b
agesa3 <collinsagesa2000@gmail.com>
2022-03-28 22:34:16 +0300

    bottom navigation and fragment setup done

--- app/src/main/java/com/agesadev/agriproject/MainActivity.java
+++ app/src/main/java/com/agesadev/agriproject/MainActivity.java
@@ -1,8 +1,13 @@
 package com.agesadev.agriproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
