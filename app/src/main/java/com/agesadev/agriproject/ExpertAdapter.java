7c5bacf7d18b4409ea048a8078bb927a7466e7fc
bienem <83782659+bienem@users.noreply.github.com>
2022-04-12 19:08:18 +0300

    experts update

---
+++ app/src/main/java/com/agesadev/agriproject/ExpertAdapter.java
@@ -0,0 +1,59 @@
+package com.agesadev.agriproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ExpertAdapter extends RecyclerView.Adapter<ExpertAdapter.viewHolder> {
+
+    ArrayList<Experts> arrayList;
+    Context context;
+
+    public ExpertAdapter(ArrayList<Experts> arrayList,Context context){
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view,parent,false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+        Experts experts = arrayList.get(position);
+        holder.eName.setText(experts.geteName());
+        holder.region.setText(experts.getRegion());
+        holder.contact.setText(experts.getContact());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+        TextView eName,region,contact;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            eName = itemView.findViewById(R.id.eName);
+            region = itemView.findViewById(R.id.region);
+            contact = itemView.findViewById(R.id.eContact);
+
+        }
+    }
+}
--- app/src/main/java/com/agesadev/agriproject/ui/ExpertsFragment.java
+++ app/src/main/java/com/agesadev/agriproject/ui/ExpertsFragment.java
@@ -2,19 +2,39 @@ package com.agesadev.agriproject.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
+import com.agesadev.agriproject.ExpertAdapter;
+import com.agesadev.agriproject.Experts;
 import com.agesadev.agriproject.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.ArrayList;
 
 
 public class ExpertsFragment extends Fragment {
 
-    
+    DatabaseReference databaseReference;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter rAdapter;
+    ArrayList<Experts> arrayList = new ArrayList<>();
+    ArrayAdapter<Experts> arrayAdapter;
+    Experts experts;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,9 +45,45 @@ public class ExpertsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_experts, container, false);
 
 
+        View view = inflater.inflate(R.layout.fragment_experts, container, false);
+
+        recyclerView = view.findViewById(R.id.eList);
+        recyclerView.setHasFixedSize(true);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Experts");
+
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Object eName = ds.child("eName").getValue();
+                    Object contact = ds.child("contact").getValue();
+                    Object region = ds.child("region").getValue();
+                    Experts experts=new Experts(eName.toString(),region.toString(),contact.toString());
+                    arrayList.add(experts);
+
+                }
+
+                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL);
+                dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.divider));
+                recyclerView.addItemDecoration(dividerItemDecoration);
+
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                rAdapter = new ExpertAdapter(arrayList, getActivity());
+                recyclerView.setAdapter(rAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        return view;
     }
 }
