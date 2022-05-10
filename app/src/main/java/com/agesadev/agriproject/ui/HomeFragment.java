a4ab5cd6d3ead04b7bc703589b6bea3fa48c4fab
agesa3 <collinsagesa2000@gmail.com>
2022-05-11 02:08:50 +0300

    able to pass data from searchview to search fragment

--- app/src/main/java/com/agesadev/agriproject/ui/HomeFragment.java
+++ app/src/main/java/com/agesadev/agriproject/ui/HomeFragment.java
@@ -2,8 +2,11 @@ package com.agesadev.agriproject.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,6 +26,7 @@ import com.agesadev.agriproject.network.ApiClient;
 import com.agesadev.agriproject.network.ApiInterface;
 
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -49,10 +53,10 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         homeRecyclerview = view.findViewById(R.id.homeRecyclerview);
-        homeProgressBar=view.findViewById(R.id.progressBarHome);
-        loadingText=view.findViewById(R.id.loadingText);
-        retryButton=view.findViewById(R.id.retryButton);
-        tipsSearchView=view.findViewById(R.id.searchTips);
+        homeProgressBar = view.findViewById(R.id.progressBarHome);
+        loadingText = view.findViewById(R.id.loadingText);
+        retryButton = view.findViewById(R.id.retryButton);
+        tipsSearchView = view.findViewById(R.id.searchTips);
 
         homeRecyclerview.setLayoutManager(new LinearLayoutManager(view.getContext()));
         makeAPICall();
@@ -62,22 +66,44 @@ public class HomeFragment extends Fragment {
     }
 
     private void handleUserSearch() {
+        tipsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                passTheSearchQuery(query);
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
     }
+
+    private void passTheSearchQuery(String query) {
+        String searchQuery = query.toLowerCase().trim();
+//        Toast.makeText(getContext(), "The query is " + searchQuery, Toast.LENGTH_SHORT).show();
+        Bundle bundle = new Bundle();
+        bundle.putString("query", searchQuery);
+        Navigation.findNavController(tipsSearchView).navigate(R.id.action_homeFragment_to_searchResults, bundle);
+    }
+
 
     private void makeAPICall() {
         apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
         apiInterface.getRecommendedTips().enqueue(new Callback<List<TipsModel>>() {
             @Override
             public void onResponse(Call<List<TipsModel>> call, Response<List<TipsModel>> response) {
-                Log.d("Log Response", "onResponse: "+ response.body());
-                //check if response contains data
-                if(response.body()!=null){
-                    homeTipsRecommendations=response.body();
+                Log.d("Log Response", "onResponse: " + response.body());
+                //check if response array length is greater than 0
+                if (response.body().size() > 0) {
+                    homeTipsRecommendations = response.body();
                     homeProgressBar.setVisibility(View.GONE);
                     loadingText.setVisibility(View.GONE);
-                    homeRecyclerview.setAdapter(new HomeTipsRecyclerViewAdapter(homeTipsRecommendations,getContext()));
-                }else{
+                    homeRecyclerview.setAdapter(new HomeTipsRecyclerViewAdapter(homeTipsRecommendations, getContext()));
+                } else {
                     Toast.makeText(getContext(), "No data found", Toast.LENGTH_SHORT).show();
                     retryButton.setVisibility(View.VISIBLE);
                 }
@@ -86,13 +112,12 @@ public class HomeFragment extends Fragment {
 
             @Override
             public void onFailure(Call<List<TipsModel>> call, Throwable t) {
-                Toast.makeText(getContext(), "Network Error " +t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Network Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     public void setUpRecyclerView() {
-
 
 
     }
